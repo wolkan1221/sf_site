0d9f2086b78448a37d22e4be43e12153ce389dac
Lytysov Artem <LytysovAS@irb.inside>
2024-04-12 13:29:44 +0300

    new sleep 0.8

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 def main() -> None:
     for i in "Hello World":
         print(i +" ", end=" ", flush=True)
-        time.sleep(0.3)
+        time.sleep(0.8)
 
 
 if __name__ == "__main__":
